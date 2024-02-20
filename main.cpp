40eacc6f76442f9e6076108f2949108ce16a0627
unknown <davidsoaita24@gmail.com>
2024-02-20 11:43:16 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    struct Student {
+    char* nume;
+    int roll_numar;
+    int varsta;
+    double total_note;
+};
+
+    return 0;
+}
