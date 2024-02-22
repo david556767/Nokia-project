1d2df094a2dd7b9ee4e175717bff523996120295
unknown <davidsoaita24@gmail.com>
2024-02-22 10:29:58 +0200

    second commit message

--- main.cpp
+++ main.cpp
@@ -2,14 +2,28 @@
 
 using namespace std;
 
-int main()
 {
-    struct Student {
-    char* nume;
-    int roll_numar;
-    int varsta;
-    double total_note;
-};
+    const int NR_CURSURI = 5;
+
+class Student {
+public:
+   string nume;
+   int an;
+   vector<double> cursuri;
+   vector<double> note;
+   double media;
+
+   Student(string nume, int an) : nume(nume), an(an) {
+       cursuri.resize(NR_CURSURI);
+       note.resize(NR_CURSURI);
+       media = calculare_media();
+   }
+
+   double calculare_media() {
+       double sum = 0;
+       for (int i = 0; i < NR_CURSURI; i++) {
+           sum += note[i];
+
 
     return 0;
 }
